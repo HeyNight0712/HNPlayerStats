9b321f50be4c0fb440d5fd7914f66c392b6d7c52
blacknight0712 <blacknight0981@gmail.com>
2024-02-09 22:42:48 +0800

    Initial commit

---
+++ src/main/java/heyblock0712/hnplayerstats/HNPlayerStats.java
@@ -0,0 +1,17 @@
+package heyblock0712.hnplayerstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HNPlayerStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
